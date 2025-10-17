3a1ec05e7846700bb404a2a381553cd9bd274715
desertlee123 <nahueld081@gmail.com>
2025-10-17 02:37:10 -0300

    primer commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController; // agregado
+
+/* Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum'); */
+
+// Mis nuevas rutas, Rutas públicas (sin token)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas protegidas (requieren token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
